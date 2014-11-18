2bb20d700cdbc9440ce9034f69934c43ef21ea65
alexchiguire <drpantalonesfelices@gmail.com>
2014-11-18 09:41:41 -0430

    Corrección de error de sintaxis en minar_tablero.js

--- js/minar_tablero.js
+++ js/minar_tablero.js
@@ -2,7 +2,7 @@ function minarTablero(tablero) {
 var contador;
 //Dentro de la variable, llamo a un número aleatoria del 1 al 10. Si el número excede 5, entonces se inserta una mina. Hasta alcanzar las 10.
 
-	for (var i = 1; 1 <= 10; i++ {
+	for (var i = 1; 1 <= 10; i++) {
 		for (var j = 1; i <=10; j++) {
 			var contador = Math.floor((Math.random() * 10) + 1);
 				if (contador > 5 && max < 10);
